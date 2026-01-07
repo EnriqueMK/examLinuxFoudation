f7fb753b7f788dc60a40e4a0c762e59d52bdc8bc
Enrique PN <132113851+EnriqueMK@users.noreply.github.com>
2026-01-07 01:41:50 -0300

    Iniciando biblioteca

--- testando.java
+++ testando.java
@@ -1,4 +1,7 @@
+import java.util.Scanner;
+
 public class testando {
+    public static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         System.out.println("Codando usando a branch backend");
     }
