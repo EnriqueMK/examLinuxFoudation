cd255b3b69925a2af1a64d5dc1e76d8c6387a2e1
Enrique PN <132113851+EnriqueMK@users.noreply.github.com>
2026-01-08 17:11:06 -0300

    recomeçando

--- index2.java
+++
@@ -1,4 +0,0 @@
-lkajsdlkj
-commite de novo
-commite novamente
-commitando para marcar a tag da versão 2 do projeto     
--- testando.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-
-public class testando {
-    public static Scanner scanner = new Scanner(System.in);
-    public static void main(String[] args) {
-        System.out.println("Codando usando a branch backend");
-    }
-}
